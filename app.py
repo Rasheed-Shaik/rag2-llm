8ad2e03d9e04a008db8f18c86e22f2fef1d6037f
Rasheed Shaik <rasheedshaik@Rasheeds-MacBook-Air.local>
2025-01-02 11:49:33 -0800

    more

--- app.py
+++ app.py
@@ -17,7 +17,12 @@ from pathlib import Path
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.schema import HumanMessage, AIMessage
 from rag_methods import stream_llm_response, stream_llm_rag_response, load_doc_to_db, load_url_to_db, initialize_documents
+import importlib
+
+print(f"Module name of load_doc_to_db: {load_doc_to_db.__module__}")
+rag_methods = importlib.import_module(load_doc_to_db.__module__)
 print(f"Imported rag_methods from: {rag_methods.__file__}")
+
 # Streamlit page configuration
 st.set_page_config(
     page_title="RAG Chat App",
